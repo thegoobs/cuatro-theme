281d97a4364baec0350f79eeb66c0fcd3e9fc145
thegoobs <guthrieschoolar95@gmail.com>
2018-06-22 11:28:40 -0500

    Fixed random hero images uploaded by site content managers in Siteleaf

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -1,13 +1,6 @@
----
----
+var num = Math.floor(Math.random() * Math.floor(4)) + 1;
+var url = $(".hero-container").attr("hero" + num);
+var css = "linear-gradient(rgba(75,75,75, 0.5),rgba(0, 0, 0, 0.9)), url(" + url + ")";
+console.log($(".hero-container").attr("hero" + num));
 
-//use liquid to get the hero image list
-var pics = {};
-{% for pics in site.hero %}
-	pics[{{forloop.index}}] = '{{pics}}';
-{% endfor %}
-
-$(document).ready(function() {
-	var p = 'assets/' + pics[Math.floor(1 + Math.random() * 4)];
-	$(".hero-container").css({"background-image": "linear-gradient(rgba(75,75,75, 0.5),rgba(0, 0, 0, 0.9)), url('" + p + "')" })
-});
+$(".hero-container").css({"background-image": css});
