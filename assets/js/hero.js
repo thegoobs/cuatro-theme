3755014cda91c4ee3277c5ac4f708c99287d2ec5
thegoobs <guthrieschoolar95@gmail.com>
2018-06-22 11:35:01 -0500

    Hotfix

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -1,6 +1,5 @@
 var num = Math.floor(Math.random() * Math.floor(4)) + 1;
 var url = $(".hero-container").attr("hero" + num);
 var css = "linear-gradient(rgba(75,75,75, 0.5),rgba(0, 0, 0, 0.9)), url(" + url + ")";
-console.log($(".hero-container").attr("hero" + num));
 
 $(".hero-container").css({"background-image": css});
